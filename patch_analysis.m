b541f9aa0c87b121679079f81cc7ca9f14f6b795
Webb <jbwebb@bcm.edu>
2018-05-24 12:14:59 -0500

    added file sorting and filtering; labeled training days

--- patch_analysis.m
+++ patch_analysis.m
@@ -4,11 +4,27 @@
 %   mouse, in numerical order
 % - plot_data: plot file data (e.g. power vs. time) while processing
 % - use_sound: use sound waveform for defining patches if position not available
-filelist = importdata('~/Desktop/matlist.txt');
+filelist = importdata('j1z4/matlist.txt');
 plot_data = false;
 use_sound = false;
 stop_thresh = 0.1;
 run_thresh = 2.0;
+
+% Sort filelist and remove irrelevant filenames
+training_days = zeros(length(filelist), 1);
+for i = 1:length(filelist)
+    filename = filelist{i};
+    [start_idx, end_idx] = regexp(filename, '_d[0-9]+_');
+    training_days(i) = str2double(filename(start_idx+2:end_idx-1));
+end 
+[sorted_days, sorted_idx] = sort(training_days);
+nan_idx = find(isnan(sorted_days));
+if ~isempty(nan_idx)
+    sorted_days = sorted_days(1:nan_idx-1);
+    sorted_idx = sorted_idx(1:nan_idx-1);
+end
+filelist = filelist(sorted_idx);
+training_days = sorted_days;
 
 % Set data placeholders for each experiment
 t_p = cell(size(filelist, 1), 1); % residence time
@@ -16,6 +32,7 @@ t_t = cell(size(filelist, 1), 1); % travel time
 r_p = cell(size(filelist, 1), 1); % total reward per patch
 d_next_patch = cell(size(filelist, 1), 1); % distance to next patch from stopping points
 d_config = zeros(size(filelist, 1), 2); % [d_patch, d_interpatch]
+keep_idx = ones(size(filelist, 1), 1); % files to include in analysis
 
 for i = 1:size(filelist)
     % Get filename
@@ -31,9 +48,20 @@ for i = 1:size(filelist)
         d_config(i, :) = [pe.d_patch, pe.d_interpatch];
     else
         % Skip file if does not contain position data
-        msg = fprintf('%s does not contain position data. Skipping file.\n', filename);
+        fprintf('%s does not contain position data. Skipping file.\n', filename);
+        keep_idx(i) = 0;
     end
 end
+
+% Remove excluded files
+keep_idx = find(keep_idx);
+filelist = filelist(keep_idx);
+training_days = training_days(keep_idx);
+t_p = t_p(keep_idx);
+t_t = t_t(keep_idx);
+r_p = r_p(keep_idx);
+d_next_patch = d_next_patch(keep_idx);
+d_config = d_config(keep_idx);
 
 %% Save data
 [start_idx, end_idx] = regexp(filename, 'j[0-9]+[a-z][0-9]+_');
@@ -41,14 +69,24 @@ end
 base_name = filename(1:end_idx);
 new_filename = [base_name, 'patch_data.mat'];
 fprintf('Saving to %s\n', new_filename);
-save(new_filename, 't_p', 't_t', 'r_p', 'd_next_patch', 'filelist');
+save(new_filename, 't_p', 't_t', 'r_p', 'd_next_patch', 'd_config', 'filelist');
 
 %% Plot patch statistics
 % Load file if not already loaded
 if ~(exist('t_p', 'var') && exist('t_t', 'var') && exist('r_p', 'var'))
     load(new_filename);
 end
-save_fig = false;
+save_fig = true;
+
+% Get training days
+if ~exist('training_days', 'var')
+    training_days = zeros(length(filelist), 1);
+    for i = 1:length(filelist)
+        filename = filelist{i};
+        [start_idx, end_idx] = regexp(filename, '_d[0-9]+_');
+        training_days(i) = str2double(filename(start_idx+2:end_idx-1));
+    end
+end
 
 % Plot patch residence times
 fig1 = figure(1);
@@ -64,6 +102,8 @@ end
 hold off;
 title('Patch Residence Time');
 xlabel('Training Day');
+xticks(1:length(filelist));
+xticklabels(training_days);
 ylabel('Time (s)');
 xlim([0 length(t_p)+1]);
 
@@ -81,6 +121,8 @@ end
 hold off;
 title('Travel Time Between Patches');
 xlabel('Training Day');
+xticks(1:length(filelist));
+xticklabels(training_days);
 ylabel('Time (s)');
 xlim([0 length(t_t)+1]);
 
@@ -98,17 +140,12 @@ end
 hold off;
 title('Average Cumulative Reward per Patch');
 xlabel('Training Day');
+xticks(1:length(filelist));
+xticklabels(training_days);
 ylabel('Reward (uL)');
 xlim([0 length(r_p)+1]);
 
-% Save figures if specified
-if save_fig
-    saveas(fig1, [base_name, 't_p']);
-    saveas(fig2, [base_name, 't_t']);
-    saveas(fig3, [base_name, 'r_p']);
-end
-
-%% Plot distribution of stopping points
+% Plot distribution of stopping points
 % histogram plots
 %{
 figure(4);
@@ -139,7 +176,8 @@ plot(edges, [zeros(1, length(edges_1)), N_2]);
 hold off;
 %}
 
-figure(6);
+fig6 = figure(6);
+clf(fig6);
 hold on;
 for i = 1:length(d_next_patch)
     idx_patch = d_next_patch{i}>=0 & d_next_patch{i}<=d_config(i, 1);
@@ -150,10 +188,18 @@ for i = 1:length(d_next_patch)
     scatter(d_next_patch{i}, ones(length(d_next_patch{i}), 1) * i, [], c);
 end
 hold off;
-fig = figure(6);
 title('Stopping Points during Training');
 xlabel('Distance (cm)');
-ylabel('Day');
+ylabel('Training Day');
+yticks(1:length(filelist));
+yticklabels(training_days);
 xlim([-max(d_config(:, 2)/2)*1.1 max(d_config(:, 1)+d_config(:, 2)/2)*1.1]);
 ylim([0 length(d_next_patch)+1]);
-saveas(fig, [base_name, 'd_stop']);
+
+%% Save figures if specified
+if save_fig
+    saveas(fig1, [base_name, 't_p']);
+    saveas(fig2, [base_name, 't_t']);
+    saveas(fig3, [base_name, 'r_p']);
+    saveas(fig6, [base_name, 'd_stop']);
+end
