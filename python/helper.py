beae1069745594efe6cb5d4b2f5676e8836c929b
jbwebb <jbwebb@bcm.edu>
2020-08-28 13:09:27 -0500

    overhauled mvt solver to increase stability

--- python/helper.py
+++ python/helper.py
@@ -107,19 +107,114 @@ def compare_patch_times(dt_patch_1, dt_patch_2, atol=1.0, rtol=0.02):
 
 ### Patch-foraging theory ###
 def _to_array(a, max_len=None):
-    if max_len is None:
-        max_len = len(a)
     if isinstance(a, np.ndarray): # broadcast if needed
-        return a * np.ones([max_len])
-    else: # assume single number
-        return np.asarray([a]) * np.ones([max_len])
+        return a
+    elif isinstance(a, (int, float)): # assume single number
+        return np.asarray([a])
+    else:
+        raise ValueError('Unable to convert type {} to ndarray.'
+                         .format(type(a)))
 
-def _solve_mvt_equation(*, F_log, F_exp, x_init, max_iter=10000):
-    try:
-        return broyden1(F_log, x_init, maxiter=max_iter) # positive solution
-    except (NoConvergence, RuntimeWarning):
-        print('Logarithmic function failed. Attempting to solve exponential function...')
-        return broyden1(F_exp, x_init) # positive solution
+def _broadcast_to_shape(a, shape, axis=None):
+    """Broadcasts array to specified shape by matching dimensions of axes."""
+    def to_tuple(name, param):
+        if isinstance(param, list):
+            return tuple(param)
+        elif isinstance(param, int):
+            return (param,)
+        else:
+            raise SyntaxError('{} must be tuple of int(s) but is type {}.'
+                              .format(name, type(shape)))
+    
+    def raise_mismatch(axis, n):
+        msg = 'Array axis {} of dimension {} does not match axis in shape parameter.' \
+              .format(axis, n)
+        raise ValueError(msg)
+    
+    # Convert parameters to proper format
+    if not isinstance(a, np.ndarray):
+        a = np.asarray(a)
+    if not isinstance(shape, tuple):
+        shape = to_tuple('shape', shape)
+    
+    s = [1]*len(shape) # rank of tensor shape
+    
+    # Match specified dimensions
+    if axis is not None: 
+        if not isinstance(axis, tuple):
+            axis = to_tuple('axis', axis)
+        if len(axis) != len(a.shape):
+            if len(axis) == 1:
+                axis = axis*len(a.shape)
+            else:
+                raise SyntaxError('Rank of input tensor and axis parameter must match.')
+        for ax, n in zip(axis, a.shape):
+            if shape[ax] != n:
+                raise ValueError('Shape and axis parameters are not compatible.')
+            else:
+                s[ax] = n
+    
+    # Find first instance of matching dimensions
+    else: 
+        for i, n in enumerate(a.shape):
+            if n > 1:
+                try:
+                    idx = shape.index(n)
+                    while s[idx] == n:
+                        idx = shape.index(n, idx+1)
+                    s[idx] = n
+                except ValueError:
+                    raise_mismatch(i, n)
+        
+    return a.reshape(s)*np.ones(shape)
+
+def random_slice(X, axis=0):
+    """Return random slice of numpy array"""
+
+    assert isinstance(X, np.ndarray)
+    
+    if X.ndim < 1:
+        return X
+    elif X.ndim < 2:
+        return X[np.random.randint(len(X))]
+    else:
+        idx = ()
+        if not isinstance(axis, tuple):
+            if isinstance(axis, list):
+                axis = tuple(axis)
+            else:
+                axis = (axis,)
+        
+        start = 0
+        for i in axis:
+            for j in range(start, i):
+                idx += (np.random.randint(X.shape[j]),)
+            idx += (slice(None),)
+            start = i + 1
+        for j in range(start, len(X.shape)):
+            idx += (np.random.randint(X.shape[j]),)
+
+        return X[idx]
+
+def _broyden1(f, x_init, max_iter=10000, min_value=-np.inf, max_value=np.inf):
+    soln = broyden1(f, x_init, maxiter=max_iter)
+    if min_value is None:
+        min_value = -np.inf
+    if max_value is None:
+        max_value = np.inf
+    if (soln < min_value).any() or (soln > max_value).any():
+        raise OverflowError('Solution outside of allowed range.')
+    return soln
+
+
+def _solve_mvt_equation(*, F_log, F_exp, x_init, verbose=False, **kwargs):
+    with np.errstate(divide='raise', invalid='raise'):
+        try:
+            return _broyden1(F_log, x_init, **kwargs) # positive solution
+        except (NoConvergence, FloatingPointError, OverflowError):
+            if verbose:
+                print('Logarithmic function failed. Attempting to solve exponential function...')
+            return _broyden1(F_exp, x_init, **kwargs) # positive solution
 
 def cumulative_reward(t_p, R_0, r_0, tau):
     return r_0 * tau * (1.0 - np.exp(-t_p / tau)) + R_0
@@ -129,7 +224,9 @@ def get_optimal_values(*, t_p=None,
                        R_0=None, 
                        r_0=None, 
                        tau=None,
-                       return_solvable=False):
+                       return_solvable=False,
+                       min_value=None,
+                       max_value=None):
     """
     Returns optimal value based on the marginal value theorem, based on values
     of other parameters.
@@ -147,9 +244,6 @@ def get_optimal_values(*, t_p=None,
     - R_opt: Amount of cumulative reward at optimal leaving time.
     - is_solvable: Array where True means data point has solution.
     """
-
-    warnings.filterwarnings('default')
-
     # Assert one unknown
     loc = [t_p, t_t, R_0, r_0, tau]
     n_unknown = len([kwarg for kwarg in loc if kwarg is None])
@@ -158,14 +252,24 @@ def get_optimal_values(*, t_p=None,
     elif n_unknown > 1:
         raise SyntaxError('Too many unknowns. Please specify all but one parameter.')
     
-    # Convert to numpy arrays for vector handling
-    max_len = max([len(kwarg) if isinstance(kwarg, np.ndarray) else 1
-                   for kwarg in loc])
-    [t_p, t_t, R_0, r_0, tau] = [_to_array(kwarg, max_len) if kwarg is not None else None 
-                                 for kwarg in loc]
+    # Convert to numpy arrays for matrix handling
+    loc = [_to_array(kwarg) if kwarg is not None else None 
+           for kwarg in loc]
+    shape = ()
+    for p in loc:
+        if p is None:
+            continue
+        elif p.ndim == 1:
+            shape += p.shape
+        else:
+            raise SyntaxError('Parameter rank must not exceed 1 (1D array)'
+                              + ' but is {}.'.format(p.ndim))
+    loc = [_broadcast_to_shape(kwarg, shape) if kwarg is not None else None 
+           for kwarg in loc]
+    [t_p, t_t, R_0, r_0, tau] = loc
 
     # Minimum travel time: R_0 / r_0
-    is_solvable = np.ones(max_len, dtype=np.bool)
+    is_solvable = np.ones(shape, dtype=np.bool)
     if (t_t is not None) and (R_0 is not None) and (r_0 is not None):
         if np.sum(t_t < (R_0 / r_0)) > 0:
             warnings.warn('Some data points exceed minimum travel time. R_0 set to zero.')
@@ -173,53 +277,105 @@ def get_optimal_values(*, t_p=None,
             R_0[idx] = 0.0
             is_solvable[idx] = False
     
-    # Solve equation based on unknown
-    if t_p is None:
-        # Solve non-linear equation for residence time (logarithmic form more numerically stable)
-        warnings.filterwarnings('error')
-        try:
-            F_log = lambda x: (np.log(r_0) - (x / tau) + np.log(t_t + x + tau) - np.log((r_0 * tau) + R_0))
-            F_exp = lambda x: (r_0 * np.exp(-x/tau) * (t_t + x + tau)) - (r_0 * tau) - R_0
-            t_p = _solve_mvt_equation(F_log=F_log, F_exp=F_exp, x_init=tau)
-        except (NoConvergence, RuntimeWarning):
-            if max_len > 1: # multiple points
-                print('Vector solution failed. Attempting to solve individual data points...')
-                t_p = np.zeros(max_len)
-                for i, [t_t_, R_0_, r_0_, tau_] in enumerate(zip(t_t, R_0, r_0, tau)):
-                    F_log = lambda x: (np.log(r_0_) - (x / tau_) + np.log(t_t_ + x + tau_) - np.log((r_0_ * tau_) + R_0_))
-                    F_exp = lambda x: (r_0_ * np.nexp(-x/tau_) * (t_t_ + x + tau_)) - (r_0_ * tau_) - R_0_
-                    t_p[i] = _solve_mvt_equation(F_log=F_log, F_exp=F_exp, x_init=t_p[i])
-        opt = t_p
-    elif t_t is None:
-        # Solve for travel time
-        t_t = ((r_0 * tau) + R_0)/(r_0 * np.exp(-t_p/tau)) + (t_p - tau)
-        opt = t_t
-    elif R_0 is None:
-        # Solve for initial reward
-        R_0 = (r_0 * np.exp(-t_p/tau) * (t_t + t_p + tau)) - (r_0 * tau)
-        opt = R_0
-    elif r_0 is None:
-        # Solve for initial rate of reward
-        if R_0 == 0.0:
-            print('Initial rate of return can be any positive number.')
-        r_0 = R_0 / (np.exp(-t_p/tau) * (t_t + t_p + tau) - tau)
-        opt = r_0
-    elif tau is None:
-        # Solve non-linear equation for decay rate (logarithmic form more numerically stable)
-        warnings.filterwarnings('error')
-        try:
-            F_log = lambda x: (np.log(r_0) - (t_p / x) + np.log(t_t + t_p + x) - np.log((r_0 * x) + R_0))
-            F_exp = lambda x: (r_0 * np.exp(-t_p/x) * (t_t + t_p + x)) - (r_0 * x) - R_0
-            tau = _solve_mvt_equation(F_log=F_log, F_exp=F_exp, x_init=t_p)
-        except (NoConvergence, RuntimeWarning):
-            if max_len > 1: # multiple points
-                print('Vector solution failed. Attempting to solve individual data points...')
-                tau = np.zeros(max_len)
-                for i, [t_p_, t_t_, R_0_, r_0_] in enumerate(zip(t_p, t_t, R_0, r_0)):
-                    F_log = lambda x: (np.log(r_0_) - (t_p_ / x) + np.log(t_t_ + t_p_ + x) - np.log((r_0_ * x) + R_0_))
-                    F_exp = lambda x: (r_0_ * np.exp(-t_p_/x) * (t_t_ + t_p_ + x)) - (r_0_ * x) - R_0_
-                    tau[i] = _solve_mvt_equation(F_log=F_log, F_exp=F_exp, x_init=t_p[i])
-        opt = tau
+    # Solve equation based on unknown.
+    # The trickiest part of solving the equation is managing the initial condition
+    # for the non-linear solver. Having the wrong initial condition can lead to
+    # overflow errors or a nonsense solution (negative or very large, since the
+    # equations can have multiple solutions). We will handle these issues with
+    # nested try-except statements, as well as iterating over different initial states.
+    with np.errstate(divide='raise', invalid='raise'): # force to raise error to catch
+        if t_p is None:
+            depth = 0 # depth of indices to iterate over
+            t_p = np.empty(shape) # initialize empty solution array
+            solved = np.zeros(shape, dtype=np.bool) # track which elements have been solved
+            while depth <= len(shape):
+                try:
+                    # Iterate over indices at depth of dimension
+                    for idx in np.ndindex(shape[:depth]):
+                        if not solved[idx].all():
+                            # Solve non-linear equation for residence time (logarithmic form more numerically stable)
+                            [t_t_, R_0_, r_0_, tau_] = [p[idx] for p in loc if p is not None ]
+                            F_log = lambda x: (np.log(r_0_) - (x / tau_) + np.log(t_t_ + x + tau_) - np.log((r_0_ * tau_) + R_0_))
+                            F_exp = lambda x: (r_0_ * np.nexp(-x/tau_) * (t_t_ + x + tau_)) - (r_0_ * tau_) - R_0_
+                            t_p[idx] = _solve_mvt_equation(F_log=F_log, F_exp=F_exp, x_init=tau_, min_value=min_value, max_value=max_value)
+                            solved[idx] = True # update successful solutions
+                    break # stop if found solution for all indices
+                except (NoConvergence, FloatingPointError, OverflowError) as error:
+                    # Catch numerical errors and try at deeper iteration
+                    depth += 1
+                    if depth > len(shape):
+                        print([t_t_, R_0_, r_0_, tau_])   
+                        raise NoConvergence('Failed to coverge to solution.')
+                    else:
+                        print('Solution failed. Attempting to solve at depth {} (of max {})...'
+                              .format(depth, len(shape)))
+            opt = t_p
+        elif t_t is None:
+            # Solve for travel time
+            t_t = ((r_0 * tau) + R_0)/(r_0 * np.exp(-t_p/tau)) + (t_p - tau)
+            opt = t_t
+        elif R_0 is None:
+            # Solve for initial reward
+            R_0 = (r_0 * np.exp(-t_p/tau) * (t_t + t_p + tau)) - (r_0 * tau)
+            opt = R_0
+        elif r_0 is None:
+            # Solve for initial rate of reward
+            if R_0 == 0.0:
+                print('Initial rate of return can be any positive number.')
+            r_0 = R_0 / (np.exp(-t_p/tau) * (t_t + t_p + tau) - tau)
+            opt = r_0
+        elif tau is None:
+            depth = 0 # depth of indices to iterate over
+            tau = np.empty(shape) # initialize empty solution array
+            solved = np.zeros(shape, dtype=np.bool) # track which elements have been solved
+            while depth <= len(shape):    
+                try:
+                    # Iterate over indices at depth of dimension
+                    for idx in np.ndindex(shape[:depth]):
+                        if not solved[idx].all():
+                            # Solve non-linear equation for decay rate (logarithmic form more numerically stable)
+                            [t_p_, t_t_, R_0_, r_0_] = [p[idx] for p in loc if p is not None]
+                            F_log = lambda x: (np.log(r_0_) - (t_p_ / x) + np.log(t_t_ + t_p_ + x) - np.log((r_0_ * x) + R_0_))
+                            F_exp = lambda x: (r_0_ * np.exp(-t_p_/x) * (t_t_ + t_p_ + x)) - (r_0_ * x) - R_0_
+                            if depth < len(shape):
+                                # Solve multidimensional equation
+                                try:
+                                    tau[idx] = _solve_mvt_equation(F_log=F_log, 
+                                                                F_exp=F_exp, 
+                                                                x_init=t_t_, 
+                                                                min_value=min_value, 
+                                                                max_value=max_value)
+                                except (NoConvergence, FloatingPointError, OverflowError) as error:
+                                    tau[idx] = _solve_mvt_equation(F_log=F_log, 
+                                                                F_exp=F_exp, 
+                                                                x_init=t_p_, 
+                                                                min_value=min_value, 
+                                                                max_value=max_value)
+                            else:
+                                # Solve individual points. Here, we can cover a reasonable space
+                                # of initial conditions (in 1D) to better find a solution.
+                                x_init = np.geomspace(0.1, max(t_p_, t_t_), num=10)
+                                for x in x_init:
+                                    try:
+                                        tau[idx] = _solve_mvt_equation(F_log=F_log, 
+                                                                    F_exp=F_exp, 
+                                                                    x_init=x, 
+                                                                    min_value=min_value, 
+                                                                    max_value=max_value)
+                                        break # stop if found solution for given x_init
+                                    except (NoConvergence, FloatingPointError, OverflowError) as error:
+                                        continue # try again with next x_init
+                            solved[idx] = True # update successful solutions
+                    break # stop if found solution for all indices
+                except (NoConvergence, FloatingPointError, OverflowError) as error:
+                    # Catch numerical errors and try at deeper iteration
+                    depth += 1
+                    if depth > len(shape):
+                        raise NoConvergence('Failed to coverge to solution.')
+                    else:
+                        print('Solution failed. Attempting to solve at depth {} (of max {})...'
+                            .format(depth, len(shape)))
+            opt = tau
     
     # Calculate total harvested reward for optimal residence time
     R_opt = cumulative_reward(t_p, R_0, r_0, tau)
